b87bb1b03d4ad400ab4fd3aa18938f9d256facce
Ronaldo Zecena <ronaldzc2018@gmail.com>
2024-05-02 21:45:38 -0600

    elecciones arregladas

--- elec.js
+++ elec.js
@@ -1,22 +1,20 @@
 
-var voto1=parseInt(document.form1.candidato1.value);
-var voto2=parseInt(document.form1.candidato2.value);
-var voto3=parseInt(document.form1.candidato3.value);
+function mostrar(){
+
+var voto1=document.form1.candidato1.value;
+var voto2=document.form1.candidato2.value;
+var voto3=document.form1.candidato3.value;
 
 var suma=parseInt(voto1)+parseInt(voto2)+parseInt(voto3);
 var porcentaje1=parseInt(voto1)/suma*100;
 var porcentaje2=parseInt(voto2)/suma*100;
 var porcentaje3=parseInt(voto3)/suma*100;
-function calcular(){
-    const xValues = ["porcentaje 1 ","porcentaje 2", "porcentaje 3"];
-    const yValues = [porcentaje1, porcentaje2, porcentaje3];
-    const barColors = [
-    "#b91d47",
-    "#00aba9",
-    "#2b5797",
-    ];
 
-    new Chart("myChart", {
+const xValues = ["Candidato 1 ","Candidato 2", "Candidato 3"];
+const yValues = [porcentaje1, porcentaje2, porcentaje3];
+const barColors = ["#b91d47","#00aba9","#2b5797",];
+
+new Chart("myChart", {
     type: "pie",
     data: {
         labels: xValues,
@@ -31,5 +29,21 @@ function calcular(){
         text: "Elecciones The Savior 2025"
         }
     }
-    });
+});
+    var tabla = "<table border='1'>";
+    tabla += "<tr><td>Resultado de las  Elecciones 2024</td></tr>";
+
+    tabla += "<tr><td>Cantidad de votos de Candidato 1:</td><td>" + voto1 + "</td></tr>";
+    tabla += "<tr><td>Porcentaje obtenido:</td><td>" + porcentaje1 + "%" + "</td></tr>";
+
+    tabla += "<tr><td>Cantidad de votos de Candidato 2:</td><td>" + voto2 + "</td></tr>";
+    tabla += "<tr><td>Porcentaje obtenido:</td><td>" + porcentaje2 + "%" + "</td></tr>";
+
+    tabla += "<tr><td>Cantidad de votos de Candidato 3:</td><td>" + voto3 + "</td></tr>";
+    tabla += "<tr><td>Porcentaje obtenido:</td><td>" + porcentaje3 + "%" + "</td></tr>";
+
+    tabla += "</table>";
+
+document.getElementById("resultados").innerHTML = tabla;
+
 }
